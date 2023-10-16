84bea9b3734a802b05de483a47bde28df297a186
YiJieChuah <s3847905@student.rmit.edu.au>
2023-10-16 14:37:13 +1100

    +next image, styles, dayjs, data fetch

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,43 @@
+import { clerkClient } from "@clerk/nextjs";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { z } from "zod";
+import { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.imageUrl,
+  };
+};
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ctx}) => {
-    return ctx.db.post.findMany();
-  })
-})
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found.",
+        });
+
+      return {
+        post,
+        author: author,
+      };
+    });
+  }),
+});
