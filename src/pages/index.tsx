6e979a7071e346283a2272015c8d2342f56f0189
YiJieChuah <s3847905@student.rmit.edu.au>
2023-10-29 15:19:40 +1100

    added toast showing zod errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,9 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -17,8 +18,16 @@ const CreatePostWizard = () => {
   const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
     onSuccess: () => {
       setInput("");
-      ctx.posts.getAll.invalidate();
-    }
+      void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if ((errorMessage)?.[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post. Please try again later.");
+      }
+    },
   });
 
   const [input, setInput] = useState<string>("");
@@ -38,10 +47,27 @@ const CreatePostWizard = () => {
         className="grow bg-transparent p-2 outline-none"
         placeholder="Type some emojis :D"
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key == "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({content: input})
+            }
+          }
+        }}
         disabled={isPosting}
         value={input}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button disabled={isPosting} onClick={() => mutate({ content: input })}>
+          Post
+        </button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20}/>
+        </div>
+      )}
     </div>
   );
 };
