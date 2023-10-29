ad0c6263ebc5088e4c57072f5055b5cc91d1c6fb
YiJieChuah <s3847905@student.rmit.edu.au>
2023-10-29 13:00:01 +1100

    rate limit implemented

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -12,6 +12,16 @@ const filterUserForClient = (user: User) => {
   };
 };
 
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+});
+
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.db.post.findMany({
@@ -50,6 +60,11 @@ export const postsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.currentUserId;
+
+      const { success } = await ratelimit.limit(authorId)
+
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" })
+
       const post = await ctx.db.post.create({
         data: {
           authorId,
