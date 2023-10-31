dcdd37f385249501257adaa40d536efa633f92a0
YiJieChuah <s3847905@student.rmit.edu.au>
2023-10-31 23:34:29 +1100

    Fixed typing issue with props in PageLayout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-const PageLayout = (props: PropsWithChildren<{}>) => {
+const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-full justify-center">
       <section className="flex min-h-screen h-full w-full flex-col border-x border-slate-400 md:max-w-2xl">
